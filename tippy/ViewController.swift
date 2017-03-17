a5d326a9b1af77f43fc04acb429e1528dbc2982f
Mhatre, Aniket <Aniket_Mhatre@intuit.com>
2017-03-17 14:39:49 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Mhatre, Aniket on 3/17/17.
+//  Copyright © 2017 Mhatre, Aniket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
